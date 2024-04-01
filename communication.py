7c97ad1d13f6e9cc7f0f3306edf9d723d39b5e25
lzhhe <67408547+lzhhe@users.noreply.github.com>
2024-04-01 14:06:46 +0800

    communication

--- communication.py
+++ communication.py
@@ -1,7 +1,53 @@
 import struct
 from enum import IntEnum
-
 import crcmod
+
+
+# 比赛类型枚举
+class GameType(IntEnum):
+    RMUL = 1  # 超级对抗赛，剩下的都没用写了防止报错
+    TEMP1 = 2
+    TEMP2 = 3
+    TEMP3 = 4
+    TEMP4 = 5
+
+
+# 比赛阶段枚举
+class GameProgress(IntEnum):
+    NOT_STARTED = 0  # 未开始
+    PREPARATION = 1  # 准备阶段
+    SELF_CHECKING_15SECONDS = 2  # 15秒自检
+    COUNTDOWN_5SECONDS = 3  # 5秒倒计时
+    IN_COMPETITION = 4  # 比赛中
+    FINISHED = 5  # 结束
+
+
+# 0x0001
+class GameStatus:
+    def __init__(self, packet):
+        unpacked_data = struct.unpack('>BHBQ', packet)
+
+        self.game_type = (unpacked_data[0] & 0xF0) >> 4
+        self.game_progress = unpacked_data[0] & 0x0F
+
+        self.stage_remain_time = unpacked_data[1]
+        self.sync_timestamp = unpacked_data[2]
+
+    # 比赛类型
+    def get_game_type_enum(self):
+        return GameType(self.game_type)
+
+    # 比赛阶段
+    def get_game_progress_enum(self):
+        return GameProgress(self.game_progress)
+
+    # 比赛剩余时间
+    def get_game_remain_time(self):
+        return self.stage_remain_time
+
+    # UNIX 时间戳
+    def get_stamp_time(self):
+        return self.sync_timestamp
 
 
 # 1:红方英雄机器人
@@ -14,7 +60,28 @@ import crcmod
 # 103/104/105:蓝方步兵机器人（与机器人 ID 3~5 对应）
 # 107:蓝方哨兵机器人
 
+class RedRobotID(IntEnum):
+    RED_HERO = 1
+    RED_ENGINEER = 2
+    RED_INFANTRY_3 = 3
+    RED_INFANTRY_4 = 4
+    RED_INFANTRY_5 = 5
+    RED_SENTRY = 7
+
+
+class BlueRobotID(IntEnum):
+    BLUE_HERO = 101
+    BLUE_ENGINEER = 102
+    BLUE_INFANTRY_3 = 103
+    BLUE_INFANTRY_4 = 104
+    BLUE_INFANTRY_5 = 105
+    BLUE_SENTRY = 107
+
+
+# 以下解包命令中packet均为解包之后去掉包头的data
+
 # 雷达状态，易伤状态 server to radar
+# 0x020C
 class RadarInfo:
     def __init__(self, packet):
         self.packet, = struct.unpack('>B', packet)
@@ -29,13 +96,14 @@ class RadarInfo:
 
 
 # 标记进度 server to radar
+# 0x020E
 class RadarMarkData:
     def __init__(self, packet):
         self.mark_hero_progress, \
             self.mark_engineer_progress, \
-            self.mark_standard_3_progress, \
-            self.mark_standard_4_progress, \
-            self.mark_standard_5_progress, \
+            self.mark_infantry_3_progress, \
+            self.mark_infantry_4_progress, \
+            self.mark_infantry_5_progress, \
             self.mark_sentry_progress = struct.unpack('6B', packet)
 
     def get_hero_progress(self):
@@ -45,79 +113,32 @@ class RadarMarkData:
         return self.mark_engineer_progress
 
     def get_standard_3_progress(self):
-        return self.mark_standard_3_progress
+        return self.mark_infantry_3_progress
 
     def get_standard_4_progress(self):
-        return self.mark_standard_4_progress
+        return self.mark_infantry_4_progress
 
     def get_standard_5_progress(self):
-        return self.mark_standard_5_progress
+        return self.mark_infantry_5_progress
 
     def get_sentry_progress(self):
         return self.mark_sentry_progress
 
-# 发送机器人坐标包  to server
+
+# 发送机器人坐标包  radar to server
+# 0x0305
 class MapRobotData:
     def __init__(self, target_robot_id, target_position_x, target_position_y):
+        self.cmd_id = 0x0305
         self.target_robot_id = target_robot_id
         self.target_position_x = target_position_x
         self.target_position_y = target_position_y
 
     def pack(self):
-        # The format string 'Hff' stands for:
-        # H: unsigned short (2 bytes, uint16_t)
-        # f: float (4 bytes) for target_position_x
-        # f: float (4 bytes) for target_position_y
-        # '>' specifies big-endian byte order. If your system uses little-endian, change it to '<'.
         return struct.pack('>Hff', self.target_robot_id, self.target_position_x, self.target_position_y)
 
     def __repr__(self):
         return f'MapRobotData({self.target_robot_id}, {self.target_position_x}, {self.target_position_y})'
-
-
-# 比赛类型枚举
-class GameType(IntEnum):
-    ROBOMASTER_UNIVERSITY_COMBAT = 1
-    ROBOMASTER_YOUTH_COMBAT = 2
-    ICRA_ROBOMASTER = 3
-    ROBOMASTER_UNIVERSITY_ENGINEERING = 4
-    ROBOMASTER_YOUTH_ENGINEERING = 5
-
-
-# 比赛阶段枚举
-class GameProgress(IntEnum):
-    NOT_STARTED = 0
-    PREPARATION = 1
-    SETUP = 2
-    IN_PROGRESS = 3
-    PAUSED = 4
-    FINISHED = 5
-
-
-# 比赛状态枚举
-class GameStatus:
-    def __init__(self, packet):
-        unpacked_data = struct.unpack('>BHBQ', packet)
-
-        self.game_type = (unpacked_data[0] & 0xF0) >> 4
-        self.game_progress = unpacked_data[0] & 0x0F
-
-        self.stage_remain_time = unpacked_data[1]
-        self.sync_timestamp = unpacked_data[2]
-
-    # 比赛类型
-    def get_game_type_enum(self):
-        return GameType(self.game_type)
-        # 比赛剩余时间
-
-    def get_game_progress_enum(self):
-        return GameProgress(self.game_progress)
-
-    def get_game_remain_time(self):
-        return self.stage_remain_time
-
-    def get_stamp_time(self):
-        return self.sync_timestamp
 
 
 class PacketBuilder:
@@ -156,3 +177,49 @@ class PacketBuilder:
 
         # Complete frame
         return frame_without_crc16 + frame_tail
+
+
+class PacketParser:
+    def __init__(self):
+        self.crc8_func = crcmod.mkCrcFun(0x107, initCrc=0x00, xorOut=0x00)
+        self.crc16_func = crcmod.mkCrcFun(0x11021, initCrc=0x00, xorOut=0x00)
+
+        # 创建命令ID到处理函数的映射
+        self.cmd_id_map = {
+            0x0001: GameStatus,
+            0x020C: RadarInfo,
+            0x020E: RadarMarkData,
+        }
+
+    def parse_packet(self, packet):
+        if len(packet) < 7:
+            raise ValueError("Incomplete packet: too short for header, CRC8, and CRC16.")
+
+        # 解析包头
+        sof, data_length, seq, crc8_received = struct.unpack('>BHB', packet[:5])
+        if sof != 0xA5:
+            raise ValueError("Invalid start of frame byte.")
+
+        if crc8_received != self.crc8_func(packet[:4]):
+            raise ValueError("CRC8 verification failed.")
+
+        data_end_pos = 5 + data_length
+        if len(packet) < data_end_pos + 2:
+            raise ValueError("Incomplete packet: Data length does not match.")
+
+        crc16_received = struct.unpack('>H', packet[data_end_pos:data_end_pos + 2])[0]
+        if crc16_received != self.crc16_func(packet[:data_end_pos]):
+            raise ValueError("CRC16 verification failed.")
+
+        # 解析cmd id 和数据
+        cmd_id = struct.unpack('>H', packet[5:7])[0]
+        data = packet[7:data_end_pos]
+
+        return self.analyse_data(cmd_id, data)
+
+    def analyse_data(self, cmd_id, data):
+        if cmd_id in self.cmd_id_map:
+            return self.cmd_id_map[cmd_id](data)
+        else:
+            print(f"Unknown command ID: {cmd_id}")
+            return None
